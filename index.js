dc63a63721ccfe2bef5c78b8314888a66e840402
Anik <naimur.rezaa@gmail.com>
2023-05-19 17:40:39 +0600

    delete toy bug fixed

--- index.js
+++ index.js
@@ -60,6 +60,16 @@ async function run() {
       res.send(result);
       // console.log(email);
     });
+
+    // delete from my toys
+    app.delete("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(id);
+      const result = await toysDb.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
